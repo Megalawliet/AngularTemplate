b2a6ef24f15363b0886db36d48ecc9a33042c7ef
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-03-23 01:30:56 +0100

    Ejercicio realizado con exito 01:30

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotComponent } from './Login/forgot/forgot.component';
+import { LoginComponent } from './Login/login/login.component';
+import { RegistrarseComponent } from './Login/registrarse/registrarse.component';
+import { MainComponent } from './Main/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: 'login',component:LoginComponent},
+  {path: 'login/forgot', component:ForgotComponent},
+  {path: 'login/registrarse', component:RegistrarseComponent},
+  {path: 'main', component:MainComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './Main/main/main.component';
+import { HomeComponent } from './Home/home/home.component';
+import { LoginComponent } from './Login/login/login.component';
+import { ForgotComponent } from './Login/forgot/forgot.component';
+import { RegistrarseComponent } from './Login/registrarse/registrarse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomeComponent,
+    LoginComponent,
+    ForgotComponent,
+    RegistrarseComponent
   ],
   imports: [
     BrowserModule,
